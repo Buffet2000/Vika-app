15c2499fd804d4678956b439d2a8331c4897b6ee
Yury Fedulov <buffet2000@gmail.com>
2026-01-19 20:26:08 +0200

    fix: fix for router

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,9 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './components/Auth/AuthProvider.jsx'
 
-// ✅ GitHub Pages SPA fallback
-const params = new URLSearchParams(window.location.search)
-const redirect = params.get('redirect')
-if (redirect) {
-  window.history.replaceState(null, '', redirect)
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
